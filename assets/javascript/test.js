c87fad42b073964f3ad9f5974ce393eff47d872c
blueDogs <danielchicchon@gmail.com>
2019-06-09 15:04:01 -0700

    Added link /to-do-list/ to navbar. Also removed p tag from card

--- assets/javascript/test.js
+++ assets/javascript/test.js
@@ -1,1 +1,22 @@
-$("#weather").hide();
+
+$('#transport-tab').on('click',function() {
+    var title = $('<h1>')
+    title.text('Transportation')
+    $('#widget-title').html(title);
+
+})
+$('#weather-tab').on('click',function() {
+    var title = $('<h1>')
+    title.text('Weather')
+    $('#widget-title').html(title);
+})
+$('#events-tab').on('click',function() {
+    var title = $('<h1>')
+    title.text('Events')
+    $('#widget-title').html(title);
+})
+$('#food-tab').on('click',function() {
+    var title = $('<h1>')
+    title.text('Transportation')
+    $('#widget-title').html(title);
+})
