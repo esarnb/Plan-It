58af35da731a098792f4d7da5312edb4e86b565a
blueDogs <danielchicchon@gmail.com>
2019-06-09 15:36:56 -0700

    added code for transportation tab to show a table based on the transportation type and status

--- assets/javascript/test.js
+++ assets/javascript/test.js
@@ -1,8 +1,30 @@
 
+
 $('#transport-tab').on('click',function() {
     var title = $('<h1>')
     title.text('Transportation')
     $('#widget-title').html(title);
+    // Make a table
+    var content = $('<table>');
+
+    // Adding Row for Table Headers
+    var contentRowHeader = $('<tr>')
+    var contentHeader = $('<th>Transport</th>')
+    contentRowHeader.append(contentHeader)
+
+
+    // Adding Data to table Headers
+    var contentRowData = $('<tr>')
+    var contentData = $('<td>Bus 48</td>')
+    contentRowData.append(contentData)
+
+    content.append(contentRowHeader)
+    content.append(contentRowData)
+
+    // Appending Content to Content Area
+    $('#content').append(content)
+
+    
 
 })
 $('#weather-tab').on('click',function() {
