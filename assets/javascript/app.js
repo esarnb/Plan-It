e1a7b7c64aa1618227e4bb5bb4d468263c9ea36b
Esar Behlum <esarnb@gmail.com>
2019-06-10 11:31:04 -0700

    Front-End ids connect with Back-end, fixed front-end dual cards, added functionality to btns, clean version commit

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,12 @@
-//Global Functions
+// //Global Functions
 
-//Functions
+// //Functions
+
+$(document).ready(function () {
+    stationNameButton();
+    getLocal();
+})
+
 // ajax call for calendarific api data on click
 $("button").on("click", function () {
 
@@ -19,50 +25,84 @@ $("button").on("click", function () {
 
 })
 // ajax call for bart api on click for user input current station
-$("button").on("click", function () {
-    // user input destination variable, not sure if we are gonna use this or a dropdown with all the stations already listed
-    var bartStation = $(this).attr("data-bart");
+// $("button").on("click", function () {
+// user input destination variable, not sure if we are gonna use this or a dropdown with all the stations already listed
+// var bartStation = $(this).attr("data-bart");
 
-    var bartQuery = "http://api.bart.gov/api/etd.aspx?cmd=etd&orig=" + bartStation + "&json=y";
+//     var bartQuery = "http://api.bart.gov/api/etd.aspx?cmd=etd&orig=" + bartStation + "&json=y";
 
-    $.ajax({
-        url: bartQuery,
-        method: "GET"
-    })
-        .then(function (root) {
-            console.log(root)
+//     $.ajax({
+//         url: bartQuery,
+//         method: "GET"
+//     })
+//         .then(function (root) {
+//             console.log(root)
 
-            var bartResults = root
+//             var bartResults = root
 
-            $("#bart-results").text(bartResults)
+//             $("#bart-results").text(bartResults)
 
-        })
+//         })
 
-})
+// })
 
 function stationNameButton() {
-   
-        $.ajax({
-            url: "http://api.bart.gov/api/stn.aspx?cmd=stns&key=MW9S-E7SL-26DU-VV8V&json=y",
-            method: "GET"
+
+    $.ajax({
+        url: "http://api.bart.gov/api/stn.aspx?cmd=stns&key=MW9S-E7SL-26DU-VV8V&json=y",
+        method: "GET"
+    })
+        .then(function (response) {
+            console.log(response)
+            var stationName = response.root.stations.station;
+            // console.log(stationName)
+            for (var i = 0; i < stationName.length; i++) {
+
+                var statName = response.root.stations.station[i].name;
+                // console.log(i)
+                var newOption = $("<option>")
+                newOption.addClass("station-button")
+                newOption.attr("name-value", statName)
+                newOption.text(statName)
+                $("#select-form").append(newOption)
+
+            }
         })
-            .then(function (root) {
-                
-                var stationName;
-    
-                for (var i = 0; i < stationName.length; i++) {
+}
 
-                    stationName = root.stations.station[i];
+// $("#submit-transport").on("click", function () {
+//     var namesQuery = "http://api.bart.gov/api/stn.aspx?cmd=stns&key=MW9S-E7SL-26DU-VV8V&json=y";
+//     var pickedPlace = $("#select-form").val().trim();
+//     $.ajax({
+//         url: namesQuery,
+//         method: "GET"
+//     })
+//         .then(function (response) {
+//             // console.log(response);
+//             var jsObjects = response.root.stations.station
+//             var result = jsObjects.filter(obj => {
+//                 return obj.name === pickedPlace
+//             })
+//             var abbrev = result[0].abbr;
+//             console.log(abbrev);
+//             /////////Second Query////////
+//             var abbrQuery = "http://api.bart.gov/api/etd.aspx?cmd=etd&orig=" + abbrev + "&key=MW9S-E7SL-26DU-VV8V&json=y";
+//             $.ajax({
+//                 url: abbrQuery,
+//                 method: "GET"
+//             })
+//                 .then(function (response) {
+//                     console.log(response);
+//                     var bartInfo = response.root.station.etd[i]
+//                     console.log(bartInfo)
+//                     $("#transport-table").text(bartInfo)
 
-                    var newOption = $("<option>")
-                    newOption.addClass("station-button")
-                    newOption.attr("name-value", stationName)
-                    newOption.text(stationName)
-                    $("#exampleFormControlSelect1").append(newOption)
-    
-                }
-            })
-    }
+
+//                 })
+//         })
+// })
+
+
 
 
 $("button").on("click", function () {
@@ -105,12 +145,25 @@ $(document).ready(function (sendRequest) {
             $("#bart-status").text(bartStatus)
 
         })
-    });
-            
+});
 
 
 
-// yelp api
+
+function getLocal() {
+    var x = document.getElementById("show-local");
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+    } else {
+        x.innerHTML = "Geolocation is not supported by this browser.";
+    }
+
+    function showPosition(position) {
+        x.innerHTML = "Latitude: " + position.coords.latitude +
+            "<br>Longitude: " + position.coords.longitude;
+    }
+
+}
 
 
 
@@ -123,6 +176,176 @@ $(document).ready(function (sendRequest) {
 
 
 /* Above, Matt only works. Below, Esar only works.    Stubbing. */
+/* Front End JS*/
+
+// Hide initial tables
+
+// TRANSPORTATION TAB //
+
+// // On click, the transportation tab will show
+$('#transport-tab').on('click',function() {
+   
+    // The title will be Transportation and added to the id widget title
+
+    $('#widget-title').text('Transportation');
+
+    //ELEMENTS
+    $('#widget-input').empty() 
+    var transportForm = $('<div class="form-group">')
+    transportForm.append($('<label for="select-form>Please Select a Station</label>'))
+
+    // ID for the Station Input
+
+    var transportSelect = ($('<select class ="form-control" id = "select-form">'))
+    transportSelect.append($('<option value="Station-1">Station One</option>'))
+    transportSelect.append($('<option value="Station-2">Station Two</option>'))
+    transportForm.append(transportSelect)
+    $('#widget-input').append(transportForm)
+
+    $('#widget-button').empty()
+    var transportButton = $('<button type = "button" class="btn btn-primary" id = "submit-transport">Submit</button>')
+    $('#widget-button').append(transportButton)
+
+    $('#transport-submit').on('click', function(event) {
+        event.preventDefault()
+
+        var stationInput = $('option').val()
+        console.log(stationInput)
+    })
+})
+
+// $('#submit-transport').on('click', function() {
+//     $('#transport-table').show()
+//     var row = $('<tr>');
+//     row.append($('<td>').text('Train T'))
+//     row.append($('<td>').text('On Time'))
+//     $('table').append(row)
+// });
+    
+    // for (i=0;i<train.length;i++) {
+    //     var contentData = $('<th>'+train[i]+'</th><th>'+status[i]+'</th>')
+    //     contentRowData.append(contentData)
+    // }
+    // $('#transport-table').show()
+
+    // Appending items to table
+    
+    // $('table').append(contentRowHeader)
+    // $('table').append(contentRowData)
+
+// WEATHER TAB //
+
+$('#weather-tab').on('click',function() {
+   
+    $('#widget-title').text('Weather');
+    $('#widget-input').empty() 
+    var weatherInput = $('<label for="location-input">Please Enter a Location</label>') 
+    weatherInput.append($('<input type="text" id = "location-input" placeholder="city,country">'))
+    $("#widget-input").append(weatherInput)
+
+    $('#widget-button').empty();
+    var weatherButton = ('<button type = "button" class="btn btn-primary" id = "location-submit">Submit</button>')
+    $('#widget-button').append(weatherButton)
+
+    $('#widget-display').empty()
+
+    $('#location-submit').on('click',function(event) {
+     
+        event.preventDefault();
+
+        var locationInput = $('#location-input').val().trim()
+        console.log(locationInput)
+        // This is our API key. Add your own API key between the ""
+        var APIKey = "fb510d3360292806c424e84f2751add1";
+
+        // Here we are building the URL we need to query the database
+        var queryURL = "https://api.openweathermap.org/data/2.5/weather?q="+locationInput+"&appid=" + APIKey;
+
+        // We then created an AJAX call
+        $.ajax({
+        url: queryURL,
+        method: "GET"
+        }).then(function(response) {
+        console.log(response)
+
+        var kelvin = response.main.temp
+
+        var fah = (kelvin-273.15)*1.80+32
+        var locationTag = $('<h3>')
+        locationTag.append(locationInput)
+
+        
+        $('#widget-display').append(locationTag)
+        var tempTag = $('<p>')
+        tempTag.append('Temperature: '+fah)
+        $('#widget-display').append(tempTag)
+
+        });
+    })
+})
+
+$('#events-tab').on('click',function() {
+    var title = $('<h1>')
+    title.text('Events')
+    $('.card-title').html(title);
+})
+
+$('#food-tab').on('click',function() {
+    
+    var title = $('<h1>')
+    title.text('Food')
+    $('.card-title').html(title);
+
+    $('#food-submit').on('click',function(event) {
+     
+        event.preventDefault();
+
+        var foodInput = $('#food-input').val().trim()
+        console.log(foodInput)
+        // This is our API key. Add your own API key between the ""
+        var APIKey = "bnRdt6tABPwVy-_r8VJsslJ50Fpx44t18Ks5srqJTsQxv2cHZuB_UqX1Fp0XSKJVmjGIQkMRgEm-ve7qXU1I3yX0xNvH_IJo-h83WtIhb9DfhHIXcaW0l_zPQ9_9XHYx";
+
+        // Here we are building the URL we need to query the database
+        var queryURL = "https://api.yelp.com/v3/businesses/search?&location="+foodInput
+        console.log(queryURL)
+        var heroku = 'https://cors-anywhere.herokuapp.com/'
+        // We then created an AJAX call
+        $.ajax({
+        url: heroku+queryURL,
+        headers: {
+            'Authorization': 'Bearer '+APIKey
+        },
+        method: "GET"
+        }).then(function(response) {
+        console.log(response)
+        
+        var business = response.businesses
+        for (var i = 0;i< business.length;i++) {
+            var businessName = response.businesses[i].name
+            var businessImage = response.businesses[i].image_url
+            
+            var businessImageDiv = $('<img src ='+ businessImage+'>')
+            
+            
+            var businessDiv = $('<h4>')
+            businessDiv.append(businessName)
+            
+            $('#foodDisplay').append(businessDiv)
+            $('#foodDisplay').append(businessImageDiv)
+        }
+
+        });
+
+    })
+})
+
+
+/*
+
+        AUTHENTICATION VARIABLES
+
+*/
+
 
 //Configurations for storage
 // Your web app's Firebase configuration
@@ -143,14 +366,15 @@ const auth = firebase.auth();
 const database = firebase.database();
 
 //Account buttons
-var btnSignUp = $("#btnSignUp");
-var btnLogin = $("#btnLogin");
-var btnLogOut = $("#btnLogOut");
+var btnSignUp = $("#authSignUp");
+var btnLogin = $("#authLogin");
+var btnLogOut = $("#sign-out");
 
 //Shows account errors and user notes. Can be separated if needed.
-var txtEmail = $("#txtEmail");
-var txtPassword = $("#txtPassword");
+var txtEmail = $("#exampleInputEmail1");
+var txtPassword = $("#exampleInputPassword1");
 var authPrompt = $("#authPrompt");
+
 var notesPrompt = $("#notesPrompt")
 //User data 
 var addNoteForm = $("#addNoteForm");
@@ -186,7 +410,6 @@ btnLogin.on('click', () => {
     const pass = txtPassword.val().trim();
     auth.signInWithEmailAndPassword(email, pass).catch(err => {
         console.log(err);
-
         if (err.code === "auth/user-not-found") authPrompt.text("New email detected. Make sure you have registered first!")
         else if (err.code === "auth/wrong-password") authPrompt.text("Invalid password.")
         else if (err.code === "auth/invalid-email") authPrompt.text("Invalid email format.")
@@ -196,8 +419,13 @@ btnLogin.on('click', () => {
 
 //Log out of the site
 btnLogOut.on('click', () => {
-    auth.signOut();
-    console.log('logged out')
+    if (auth.currentUser) {
+        auth.signOut();
+        console.log('logged out')
+    }
+    else {
+        $("#exampleModal").modal("show")
+    }
 })
 
 // --------------------------- User Notes ----------------------------//
@@ -205,22 +433,15 @@ btnLogOut.on('click', () => {
 //When a user signs in/out listener
 auth.onAuthStateChanged(user => {
     if (user) {
-        /*Add/Remove a class "hide" for instant. */
-        txtEmail.addClass("d-none");
-        txtPassword.addClass("d-none");
-        btnLogin.addClass("d-none");
-        btnSignUp.addClass("d-none");
-        btnLogOut.removeClass("d-none")
-        addNote.removeClass("d-none")
+        btnLogOut.text("Logout")
+        $("#exampleModal").modal('hide');
         authPrompt.text("Planner App")
-        console.log(auth.currentUser.email + " has now logged in.");
+        console.log("User has now logged in.");
 
         //Get the current user data obj
         var userData;
         database.ref("/users").orderByChild("email").equalTo(auth.currentUser.email).once('value')
             .then(function (snapshot) {
-                console.log(snapshot.val());
-
                 userData = Object.values(snapshot.val())[0];
                 //Convert the object to a list of numbered notes and display it to the screen
                 var userNotes = userData.notes.map((perNote, index) => ((index + 1) + ". " + perNote)).join("<br>");
@@ -228,13 +449,8 @@ auth.onAuthStateChanged(user => {
             })
 
     } else {
-        txtEmail.removeClass("d-none");
-        txtPassword.removeClass("d-none");
-        btnLogin.removeClass("d-none");
-        btnSignUp.removeClass("d-none");
-        btnLogOut.addClass("d-none")
-        addNote.addClass("d-none")
         notesPrompt.empty();
+        btnLogOut.text("Login")
         console.log("User has now logged out.");
         authPrompt.text("Please Log in!")
     }
@@ -260,9 +476,7 @@ function updateUserNotes(type, note) {
             userData = Object.values(snapshot.val())[0];
             if (type === "add") userData.notes.push(note);
             else userData.notes.splice(note, 1);
-            console.log(userData.notes);
             addNote.val("");
-            console.log(auth.currentUser.uid);
 
             database.ref("/users").child(auth.currentUser.uid).update({
                 notes: userData.notes
