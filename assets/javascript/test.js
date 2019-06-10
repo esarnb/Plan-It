ddf459fae79cb32e943e0df0b1d91ac9c9bcb3d5
blueDogs <danielchicchon@gmail.com>
2019-06-09 17:17:55 -0700

    added dropdown menu for transportation tab. Also added a table for the card content once the submit button is pressed

--- assets/javascript/test.js
+++ assets/javascript/test.js
@@ -1,33 +1,41 @@
+// Hide initial table
+$('#transport-select').hide()
+$('#transport-table').hide()
 
-
+// On click, the transportation tab will show
 $('#transport-tab').on('click',function() {
+    $('#transport-select').show()
+
+    // The title will be Transportation and added to the id widget title
     var title = $('<h1>')
     title.text('Transportation')
     $('#widget-title').html(title);
-    // Make a table
-    var content = $('<table>');
 
+})
+
+$('#submit-transport').on('click', function() {
+    $('#transport-table').show()
     // Adding Row for Table Headers
     var contentRowHeader = $('<tr>')
-    var contentHeader = $('<th>Transport</th>')
+    var contentHeader = $('<th>Transport</th><th>Status<th>')
     contentRowHeader.append(contentHeader)
 
 
     // Adding Data to table Headers
     var contentRowData = $('<tr>')
-    var contentData = $('<td>Bus 48</td>')
+    var contentData = $('<td>(Place Holder for Data)</td><td>(Place Holder for Data)</td>')
     contentRowData.append(contentData)
 
-    content.append(contentRowHeader)
-    content.append(contentRowData)
-
-    // Appending Content to Content Area
-    $('#content').append(content)
-
+    // Appending items to table
     
-
+    $('table').append(contentRowHeader)
+    $('table').append(contentRowData)
 })
+
+
+
 $('#weather-tab').on('click',function() {
+    
     var title = $('<h1>')
     title.text('Weather')
     $('#widget-title').html(title);
