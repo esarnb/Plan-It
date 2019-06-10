56a3387e6f8f6f45043265e46ed473c8b5d9fce8
Minal Kharche <minal.berkeley@gmail.com>
2019-06-10 13:21:07 -0700

    Reformat client-side JavaScript.

--- assets/javascript/test.js
+++ assets/javascript/test.js
@@ -1,12 +1,12 @@
 
 // NOTES TAB //
 
-$('#notes-tab').on('click',function() {
+$('#notes-tab').on('click', function () {
     $('#widget-title').text('Notes');
     $('#widget-input').empty();
     $('#widget-button').empty();
     $('#widget-display').empty();
-    
+
     var textArea = $('<div class="form-group">')
     textArea.append($('<label for="comment">Comment:</label>'))
     textArea.append($('<textarea class="form-control" rows="1" id = "comment"></textarea>'))
@@ -15,35 +15,35 @@ $('#notes-tab').on('click',function() {
     var textButton = $('<button type = "button" class="btn btn-primary" id = "submit-text">Submit</button>')
     $('#widget-button').append(textButton)
 
-    $('#submit-text').on('click',function(event) {
+    $('#submit-text').on('click', function (event) {
         event.preventDefault();
 
         $('#widget-display-top').empty()
         var textInput = $('#comment').val()
-        
+
         textArray.push(textInput)
         console.log(textArray)
 
-        for (var i = 0; i < textArray.length;i++) {
+        for (var i = 0; i < textArray.length; i++) {
             var p = $('<p>')
             p.append(textArray[i])
             $('#widget-display-top').append(p)
         }
-        
+
     })
 })
 
 // TRANSPORTATION TAB //
 
 // // On click, the transportation tab will show
-$('#transport-tab').on('click',function() {
-   
+$('#transport-tab').on('click', function () {
+
     // The title will be Transportation and added to the id widget title
 
     $('#widget-title').text('Transportation');
 
     //ELEMENTS
-    $('#widget-input').empty() 
+    $('#widget-input').empty()
     var transportForm = $('<div class="form-group">')
     transportForm.append($('<label for="select-form>Please Select a Station</label>'))
 
@@ -59,7 +59,7 @@ $('#transport-tab').on('click',function() {
     var transportButton = $('<button type = "button" class="btn btn-primary" id = "submit-transport">Submit</button>')
     $('#widget-button').append(transportButton)
 
-    $('#transport-submit').on('click', function(event) {
+    $('#transport-submit').on('click', function (event) {
         event.preventDefault()
 
         var stationInput = $('option').val()
@@ -74,25 +74,25 @@ $('#transport-tab').on('click',function() {
 //     row.append($('<td>').text('On Time'))
 //     $('table').append(row)
 // });
-    
-    // for (i=0;i<train.length;i++) {
-    //     var contentData = $('<th>'+train[i]+'</th><th>'+status[i]+'</th>')
-    //     contentRowData.append(contentData)
-    // }
-    // $('#transport-table').show()
 
-    // Appending items to table
-    
-    // $('table').append(contentRowHeader)
-    // $('table').append(contentRowData)
+// for (i=0;i<train.length;i++) {
+//     var contentData = $('<th>'+train[i]+'</th><th>'+status[i]+'</th>')
+//     contentRowData.append(contentData)
+// }
+// $('#transport-table').show()
+
+// Appending items to table
+
+// $('table').append(contentRowHeader)
+// $('table').append(contentRowData)
 
 // WEATHER TAB //
 
-$('#weather-tab').on('click',function() {
-   
+$('#weather-tab').on('click', function () {
+
     $('#widget-title').text('Weather');
-    $('#widget-input').empty() 
-    var weatherInput = $('<label for="location-input">Please Enter a Location</label>') 
+    $('#widget-input').empty()
+    var weatherInput = $('<label for="location-input">Please Enter a Location</label>')
     weatherInput.append($('<input type="text" id = "location-input" placeholder="city,country">'))
     $("#widget-input").append(weatherInput)
 
@@ -102,8 +102,8 @@ $('#weather-tab').on('click',function() {
 
     $('#widget-display').empty()
 
-    $('#location-submit').on('click',function(event) {
-     
+    $('#location-submit').on('click', function (event) {
+
         event.preventDefault();
 
         var locationInput = $('#location-input').val().trim()
@@ -112,33 +112,33 @@ $('#weather-tab').on('click',function() {
         var APIKey = "fb510d3360292806c424e84f2751add1";
 
         // Here we are building the URL we need to query the database
-        var queryURL = "https://api.openweathermap.org/data/2.5/weather?q="+locationInput+"&appid=" + APIKey;
-        var forecastURL = "https://api.openweathermap.org/data/2.5/forecast?q="+locationInput+"&mode=xml&appid="+ APIKey;
+        var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + locationInput + "&appid=" + APIKey;
+        var forecastURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + locationInput + "&mode=xml&appid=" + APIKey;
 
         // We then created an AJAX call
         $.ajax({
-        url: queryURL,
-        method: "GET"
-        }).then(function(response) {
-        console.log(response)
+            url: queryURL,
+            method: "GET"
+        }).then(function (response) {
+            console.log(response)
 
-        var kelvin = response.main.temp
+            var kelvin = response.main.temp
 
-        var fah = (kelvin-273.15)*1.80+32
-        var locationTag = $('<h3>')
-        locationTag.append(locationInput)
+            var fah = (kelvin - 273.15) * 1.80 + 32
+            var locationTag = $('<h3>')
+            locationTag.append(locationInput)
 
-        
-        $('#widget-display').append(locationTag)
-        var tempTag = $('<p>')
-        tempTag.append('Temperature: '+fah)
-        $('#widget-display').append(tempTag)
+
+            $('#widget-display').append(locationTag)
+            var tempTag = $('<p>')
+            tempTag.append('Temperature: ' + fah)
+            $('#widget-display').append(tempTag)
         });
 
         $.ajax({
             url: forecastURL,
             method: "GET"
-            }).then(function(response) {
+        }).then(function (response) {
             console.log(response)
 
         });
@@ -146,7 +146,7 @@ $('#weather-tab').on('click',function() {
     })
 })
 
-$('#events-tab').on('click',function() {
+$('#events-tab').on('click', function () {
     $('#widget-title').text('Events');
 })
 
@@ -168,8 +168,8 @@ $('#food-tab').on('click', function () {
 
     $('#widget-display').empty()
 
-    $('#food-submit').on('click',function(event) {
-     
+    $('#food-submit').on('click', function (event) {
+
         event.preventDefault();
 
         var foodInput = $('#food-input').val().trim()
@@ -178,33 +178,33 @@ $('#food-tab').on('click', function () {
         var APIKey = "bnRdt6tABPwVy-_r8VJsslJ50Fpx44t18Ks5srqJTsQxv2cHZuB_UqX1Fp0XSKJVmjGIQkMRgEm-ve7qXU1I3yX0xNvH_IJo-h83WtIhb9DfhHIXcaW0l_zPQ9_9XHYx";
 
         // Here we are building the URL we need to query the database
-        var queryURL = "https://api.yelp.com/v3/businesses/search?&location="+foodInput
+        var queryURL = "https://api.yelp.com/v3/businesses/search?&location=" + foodInput
         console.log(queryURL)
         var heroku = 'https://cors-anywhere.herokuapp.com/'
         // We then created an AJAX call
         $.ajax({
-        url: heroku+queryURL,
-        headers: {
-            'Authorization': 'Bearer '+APIKey
-        },
-        method: "GET"
-        }).then(function(response) {
-        console.log(response)
-        
-        var business = response.businesses
-        for (var i = 0;i< business.length;i++) {
-            var businessName = response.businesses[i].name
-            var businessImage = response.businesses[i].image_url
-            
-            var businessImageDiv = $('<img src ='+ businessImage+'>')
-            
-            
-            var businessDiv = $('<h4>')
-            businessDiv.append(businessName)
-            
-            $('#widget-display').append(businessDiv)
-            $('#widget-display').append(businessImageDiv)
-        }
+            url: heroku + queryURL,
+            headers: {
+                'Authorization': 'Bearer ' + APIKey
+            },
+            method: "GET"
+        }).then(function (response) {
+            console.log(response)
+
+            var business = response.businesses
+            for (var i = 0; i < business.length; i++) {
+                var businessName = response.businesses[i].name
+                var businessImage = response.businesses[i].image_url
+
+                var businessImageDiv = $('<img src =' + businessImage + '>')
+
+
+                var businessDiv = $('<h4>')
+                businessDiv.append(businessName)
+
+                $('#widget-display').append(businessDiv)
+                $('#widget-display').append(businessImageDiv)
+            }
 
         });
 
